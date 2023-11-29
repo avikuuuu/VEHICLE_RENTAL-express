06c27cba130e7a68a1dd779c477e50535981789f
Avdesh kumar <ku.kumar.avdesh786@gmail.com>
2023-11-29 12:45:46 +0530

    new upload

---
+++ index.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const mainRouter = require("./src/routers/main");
+
+const server = express();
+const logger = morgan(
+  ":method :url :status :res[content-length] - :response-time ms"
+);
+
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+// first router
+const corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:8080",
+  ],
+  allowedHeader: "x-access-token",
+  method: ["GET", "POST", "PATCH", "DETELE", "OPTIONS"],
+};
+
+server.use(cors(corsOptions));
+server.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+server.use(express.json());
+server.use(logger);
+server.use(express.static("public/img/users"));
+server.use(express.static("public/img/vehicles"));
+server.use(mainRouter);
